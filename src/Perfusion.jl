3644f2328cb751fc2909582b55e6932d4828e84b
Zaki A <zaki@live.ca>
2019-10-06 21:04:23 -0400

    Simplify src folder by removing subfolder for aif

--- src/Perfusion.jl
+++ src/Perfusion.jl
@@ -9,7 +9,7 @@ macro extract(varnames, namedtuple)
 end
 
 using SpecialFunctions: gamma
-include("aif/literature-based.jl")
+include("aif.jl")
 export aif_biexponential, aif_fritzhansen, aif_weinmann
 export aif_orton1, aif_orton2, aif_orton3
 export aif_georgiou, aif_parker
