92574d847ba6c9d1704b188a51d3d1fdc5ac0dd1
Zaki A <zaki@live.ca>
2019-10-08 02:36:56 -0400

    Specify function when using external module

--- src/Perfusion.jl
+++ src/Perfusion.jl
@@ -1,7 +1,5 @@
 module Perfusion
 
-# Arterial input functions
-
 macro extract(varnames, namedtuple)
     ex = Expr(:block)
     ex.args = [:($(esc(var)) = getindex($(esc(namedtuple)), $(esc(QuoteNode(var))))) for var in varnames.args]
@@ -14,7 +12,7 @@ export aif_biexponential, aif_fritzhansen, aif_weinmann
 export aif_orton1, aif_orton2, aif_orton3
 export aif_georgiou, aif_parker
 
-using NumericalIntegration
+using NumericalIntegration: cumul_integrate
 include("model_fitting.jl")
 export model_tofts, fit_tofts, fit_extendedtofts
 
